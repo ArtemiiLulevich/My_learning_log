64d7abc442d9aa3d57db3f52c444a57eae29d055
Artemii <artemii.lulevich@gmail.com>
2020-01-23 19:51:10 +0200

    Refactoring and security fix

--- learning_log/learning_logs/views.py
+++ learning_log/learning_logs/views.py
@@ -23,8 +23,7 @@ def topics(request):
 def topic(request, topic_id):
     topic = Topic.objects.get(id=topic_id)
 
-    if topic.owner != request.user:
-        raise Http404
+    check_topic_owner(request, topic)
 
     entries = topic.entry_set.order_by('-date_added')
     context = {'topic': topic, 'entries': entries}
@@ -53,10 +52,12 @@ def new_entry(request, topic_id):
     topic = Topic.objects.get(id=topic_id)
 
     if request.method != 'POST':
+        check_topic_owner(request, topic)
         form = EntryForm()
     else:
         form = EntryForm(data=request.POST)
         if form.is_valid():
+
             new_entry = form.save(commit=False)
             new_entry.topic = topic
             new_entry.save()
@@ -71,8 +72,7 @@ def edit_entry(request, entry_id):
     entry = Entry.objects.get(id=entry_id)
     topic = entry.topic
 
-    if topic.owner != request.user:
-        raise Http404
+    check_topic_owner(request, topic)
 
     if request.method != 'POST':
         form = EntryForm(instance=entry)
@@ -85,3 +85,7 @@ def edit_entry(request, entry_id):
     context = {'entry': entry, 'topic': topic, 'form': form}
     return render(request, 'learning_logs/edit_entry.html', context)
 
+
+def check_topic_owner(request, topic):
+    if topic.owner != request.user:
+        raise Http404
